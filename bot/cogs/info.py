2f671218b1931dbc39a59201119e736ed0d3e290
Studio Butter <55838591+studiobutter@users.noreply.github.com>
2026-01-02 21:39:14 +0700

    Update about command

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -12,7 +12,7 @@ class InfoCog(commands.Cog):
         embed = discord.Embed(
             title="Wonderland Explorer",
             color=15844367,
-            description="This bot is made by Studio Butter ([@charaanimates](<discord://-/users/674463869816799243>)) and is open-source on [GitHub](<https://github.com/studiobutter/Wonderland_Share>). For questions or issues with this bot, DM me on Discord or open an issue on GitHub.\nTo contribute, DM me or open a PR on GitHub.\nThis bot is self-hosted in my house, so uptime is not guaranteed. If you wish to have it hosted 24/7, feel free to donate [here](<https://ko-fi.com/studiobutterteam>)",
+            description="This bot is made by Studio Butter ([@charaanimates](<discord://-/users/674463869816799243>)) and is open-source on [GitHub](<https://github.com/studiobutter/Wonderland_Share>). For questions or issues with this bot, DM me on Discord or open an issue on GitHub.\nTo contribute, DM me or open a PR on GitHub.\nThis bot is self-hosted in my house, so uptime is not guaranteed. If you wish to have it hosted 24/7, feel free to donate [here](<https://ko-fi.com/studiobutterteam>)\n\n[Join our Miliastra Discord Community](<https://discord.gg/2cJyk55Mz9>)",
         )
         await interaction.response.send_message(embed=embed)
 
